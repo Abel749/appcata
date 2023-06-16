d43f5d36992851b77743a8709a89b2766afd333b
Abel <820659749@qq.com>
2023-06-16 09:05:06 +0800

    首页链接

--- src/components/content2.jsx
+++ src/components/content2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from "next/image";
 import {Col, Row} from "antd";
 import Link from "next/link";
-
+import styles from '../styles/content2.module.css';
 const titleH1Style={
     color:'#036ED6',
     fontSize:'40px',
@@ -46,25 +46,77 @@ const Content2 = () => (
             md: 24,
             lg: 32,
         }}>
-            <Col> <Image src="/u112_state0.png" width={270} height={500} priority/>
-                <button>
-                    <Link href="/extdata">了解更多</Link>
-                </button>
+            <Col>
+                <div className={styles.container}>
+                    <div className={styles.one}>
+                        <div style={{marginTop:"330px"} }>
+                            <h3 style={{fontSize:"24px"} }>外部数据</h3>
+                        </div>
+                    </div>
+                        <div className={styles.two}>
+                            <Link href="/extdata">
+                                <div className={styles.link}>
+                                    <h3 style={{fontSize:"24px"} }>外部数据</h3>
+                                    <p className={styles.content}>全行外部数据共涉及64家供应商、109个数据项（5个终端），为全行各项经营管理、业务产品提供了622个数据产品支撑。全行24个总行部门和15家分行已经在使用外部数据，覆盖80%的部门和75%的分行</p>
+                                     <button style={{width:"100px", height:"40px"}}>了解更多</button>
+                                </div>
+                            </Link>
+                        </div>
+                </div>
             </Col>
-            <Col> <Image src="/u119_state0.png" width={270} height={500} priority/>
-                <button>
-                    <Link href="/errorpage">了解更多</Link>
-                </button>
+            <Col>
+                <div className={styles.container2}>
+                    <div className={styles.one2}>
+                        <div style={{marginTop:"330px"} }>
+                            <h3 style={{fontSize:"24px"} }>数据应用</h3>
+                        </div>
+                    </div>
+                    <div className={styles.two2}>
+                        <Link href="/errorpage">
+                            <div className={styles.link}>
+                                <h3 style={{fontSize:"24px"} }>数据应用</h3>
+                                <p className={styles.content}>全行外部数据共涉及64家供应商、109个数据项（5个终端），为全行各项经营管理、业务产品提供了622个数据产品支撑。全行24个总行部门和15家分行已经在使用外部数据，覆盖80%的部门和75%的分行</p>
+                                 <button style={{width:"100px", height:"40px"}}>了解更多</button>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
             </Col>
-            <Col> <Image src="/u126_state0.png" width={270} height={500} priority/>
-                <button>
-                    <Link href="/errorpage">了解更多</Link>
-                </button>
+            <Col>
+                <div className={styles.container2}>
+                    <div className={styles.one3}>
+                        <div style={{marginTop:"330px"} }>
+                            <h3 style={{fontSize:"24px"} }>数据治理</h3>
+                        </div>
+                    </div>
+                        <div className={styles.two3}>
+                            <Link href="/errorpage">
+                                <div className={styles.link}>
+                                    <h3 style={{fontSize:"24px"} }>数据治理</h3>
+                                    <p className={styles.content}>全行外部数据共涉及64家供应商、109个数据项（5个终端），为全行各项经营管理、业务产品提供了622个数据产品支撑。全行24个总行部门和15家分行已经在使用外部数据，覆盖80%的部门和75%的分行</p>
+                                    <button style={{width:"100px", height:"40px"}}>了解更多</button>
+                                </div>
+                            </Link>
+                        </div>
+                </div>
             </Col>
-            <Col> <Image src="/u133_state0.png" width={270} height={500} priority/>
-                <button>
-                    <Link href="/errorpage">了解更多</Link>
-                </button>
+            <Col>
+                <div className={styles.container2}>
+                    <div className={styles.one4}>
+                        <div style={{marginTop:"330px"} }>
+                            <h3 style={{fontSize:"24px"} }>数据安全</h3>
+                        </div>
+                    </div>
+                        <div className={styles.two4}>
+                            <Link href="/errorpage">
+                                <div className={styles.link}>
+                                    <h3 style={{fontSize:"24px"} }>数据安全</h3>
+                                    <p className={styles.content}>全行外部数据共涉及64家供应商、109个数据项（5个终端），为全行各项经营管理、业务产品提供了622个数据产品支撑。全行24个总行部门和15家分行已经在使用外部数据，覆盖80%的部门和75%的分行</p>
+                                    <button style={{width:"100px", height:"40px"}}>了解更多</button>
+                                </div>
+                        </Link>
+                        </div>
+                </div>
             </Col>
         </Row>
 
